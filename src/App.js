c023ecdd62d54712e285bb4a47ef211e9850547a
manibharathi <manibharathir@sugunafoods.com>
2022-12-13 15:20:42 +0530

    Wow And Animted CSS Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Animated } from 'react-animated-css'
 
-function App() {
+import WOW from "wowjs";
+
+
+class App extends React.Component {
+
+  componentDidMount() 
+  {
+    const wow = new WOW.WOW();
+    wow.init();
+  }
+
+  render()
+  {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* <img src={logo} className="App-logo wow fadeInDown" alt="logo" /> */}
+     
+        <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        </Animated>
+       
+        
         <a
-          className="App-link"
+          className="App-link wow fadeIn" data-wow-delay="0.5s"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
+         
           Learn React
         </a>
+        
       </header>
     </div>
   );
+}
 }
 
 export default App;
